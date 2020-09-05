beb4debf258d5a6630d6c16cfea1271367e21838
ekook <edselkook@gmail.com>
2020-09-05 13:52:54 -0400

    My first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\knowledge\udemy\builddjangorealproject\carzone-product\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
